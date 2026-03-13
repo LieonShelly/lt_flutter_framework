89a423fa13381dc185e3b8fae5cd12c4ecc0b549
lieon <lieoncx@gmail.com>
2026-03-13 12:26:12 +0800

    Update

---
+++ apps/algorithm_app/lib/src/sort/bubble_sort/bubble_sort.dart
@@ -0,0 +1,17 @@
+import '../../core/core.dart';
+
+void bubbleSort(List<int> arr) {
+  int n = arr.length;
+  bool swapped = false;
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+}
