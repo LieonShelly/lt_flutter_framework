d069faa1e28259152cc2286d9bd6aa88b923b5e9
lieon <lieoncx@gmail.com>
2026-03-20 18:01:53 +0800

    Update

---
+++ apps/compass_app/lib/src/di/dependencies.dart
@@ -0,0 +1,6 @@
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+// List<SingleChildWidget> _sharedProviders = [
+//   Provider(lazy: true, create: (context) => BookingC,)
+// ]
