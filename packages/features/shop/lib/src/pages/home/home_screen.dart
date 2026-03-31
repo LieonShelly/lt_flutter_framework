5a9aedd796843466aaf8d7ee5010586e41a271d5
lieon <lieoncx@gmail.com>
2026-03-31 12:05:36 +0800

    Update

--- packages/features/shop/lib/src/pages/home/home_screen.dart
+++ packages/features/shop/lib/src/pages/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop/src/constants/constants.dart';
 import 'package:shop/src/pages/home/categories/categories.dart';
 import 'package:shop/src/pages/home/flash_scale/flash_scale.dart';
+import 'package:shop/src/pages/home/most_popular/most_popular.dart';
 import 'package:shop/src/pages/home/offers_carousel/offers_carousel.dart';
 import 'package:shop/src/pages/home/popular_products/popular_products.dart';
 
@@ -25,9 +26,10 @@ class _HomeScreenState extends State<HomeScreen> {
             const SliverToBoxAdapter(child: Categories()),
             const SliverToBoxAdapter(child: PopularProducts()),
             const SliverPadding(
-              padding: EdgeInsets.symmetric(vertical: defaultPadding * 1.5),
+              padding: EdgeInsets.symmetric(vertical: defaultPadding * 0),
               sliver: SliverToBoxAdapter(child: FlashScale()),
             ),
+            const SliverToBoxAdapter(child: MostPopular()),
           ],
         ),
       ),
---
+++ packages/features/shop/lib/src/pages/home/most_popular/most_popular.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:shop/src/constants/constants.dart';
+import 'package:shop/src/pages/home/popular_products/product_card.dart';
+import 'package:shop/src/pages/home/popular_products/product_model.dart';
+
+class MostPopular extends StatelessWidget {
+  const MostPopular({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            left: defaultPadding,
+            top: defaultPadding,
+            bottom: defaultPadding,
+          ),
+          child: Text(
+            "Most popular",
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ),
+
+        SizedBox(
+          height: 220,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: demoPopularProducts.length,
+            itemBuilder: (context, index) {
+              final product = demoPopularProducts[index];
+
+              return Padding(
+                padding: EdgeInsets.only(
+                  left: index == 0 ? defaultPadding : 0,
+                  right: defaultPadding,
+                ),
+                child: ProductCard(
+                  image: product.image,
+                  brandName: product.brandName,
+                  title: product.title,
+                  price: product.price,
+                  press: () {},
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
